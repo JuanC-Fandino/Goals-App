5b1e7214d26aa62e0d02fcb339fc6ef8c5e3c29c
Juan Camilo Fandiño Orjuela <juancamilo1998@hotmail.com>
2022-12-05 15:47:43 -0500

    feat: initial learning

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.dummyText}>This is so amazing! I'm super happy
+                wow!</Text>
+            <Text style={styles.dummyText}> Wow </Text>
+            <Button title={'holi'} onPress={() => alert('holi')}/>
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    dummyText: {
+        margin: 16,
+        padding: 16,
+        borderWidth: 3,
+        borderColor: 'blue'
+    }
 });
