07baf6310582f3942f202b8a7ad0caa9db230905
Juan Camilo Fandiño Orjuela <juancamilo1998@hotmail.com>
2022-12-06 14:40:58 -0500

    feat: style component

--- App.js
+++ App.js
@@ -1,29 +1,89 @@
-import {StatusBar} from 'expo-status-bar';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import { useState } from 'react';
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+
+// Every view organizes its children into a column by default (Flexbox)
+// Child elements are organized
+// JustifyContent: aligns children in the main axis
+// The style doesn't cascade down to the children
 
 export default function App() {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.dummyText}>This is so amazing! I'm super happy
-                wow!</Text>
-            <Text style={styles.dummyText}> Wow </Text>
-            <Button title={'holi'} onPress={() => alert('holi')}/>
-            <StatusBar style="auto"/>
-        </View>
-    );
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goalHandler() {
+    setCourseGoals([...courseGoals, enteredGoalText]);
+  }
+
+  function changeTextHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="La meta que deseas alcanzar"
+          onChangeText={changeTextHandler}
+        />
+        <Button title="Agregar" onPress={goalHandler} />
+      </View>
+      <View style={styles.listContainer}>
+        <ScrollView>
+          {courseGoals.length !== 0 &&
+            courseGoals.map((goal) => (
+              <View style={styles.goalItem}>
+                <Text style={styles.goalText} key={goal}>
+                  {goal}
+                </Text>
+              </View>
+            ))}
+        </ScrollView>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    dummyText: {
-        margin: 16,
-        padding: 16,
-        borderWidth: 3,
-        borderColor: 'blue'
-    }
+  appContainer: {
+    padding: 50,
+    paddingHorizontal: 16,
+    height: '100%',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#abab00',
+    flex: 1,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#abab00',
+    width: '70%',
+    marginRight: 8,
+    padding: 8,
+  },
+  listContainer: {
+    flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#abab00',
+    color: 'white',
+  },
+  goalText: {
+    color: 'white',
+  },
 });
